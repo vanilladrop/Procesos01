bdc013e140f55956e55023409a79cbbf817268e9
CreamySweets <victorfc_1804@hotmail.com>
2020-02-18 12:10:50 -0600

    Proyecto de factorial en Python uwu

---
+++ Factorial.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return factorial(n-1) * n
+
+num = int(input("Escribe el número del que requieres su factorial: "))
+
+if num < 0:
+    print("No hay factorial para números negativos.")
+elif num == 0:
+    print("El factorial de 0 es 1.")
+else:
+    print("El factorial de", num, "es", factorial(num))
